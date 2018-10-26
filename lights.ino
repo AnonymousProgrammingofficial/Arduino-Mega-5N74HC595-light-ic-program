0eecdd05b226d7fcc7832cf6de234ac3f6ef7473
Anonymous Programming Official <44509855+AnonymousProgrammingofficial@users.noreply.github.com>
2018-10-26 16:29:06 -0400

    Add files via upload

---
+++ lights.ino
@@ -0,0 +1,37 @@
+ /* by: Anonymous Programmer (JM)2018. For use with the 74hc595 IC Chip register. 
+ *  lights up eight leds using only three pins
+ *  arduino mega 2560.
+ *
+ */
+int latchMainPin = 5;
+int clockPinMain = 6;
+int dataPinMain = 4;
+
+byte Lights = 0;
+
+void setup() 
+{
+  pinMode(latchMainPin, OUTPUT);
+  pinMode(dataPinMain, OUTPUT);  
+  pinMode(clockPinMain, OUTPUT);
+}
+
+void loop() 
+{
+  Lights = 0;
+  ShiftRegister();
+  delay(300);
+  for (int j = 0; j < 8; j++)
+  {
+    bitSet(Lights, j);
+    ShiftRegister();
+    delay(300);
+  }
+}
+
+void ShiftRegister()
+{
+   digitalWrite(latchMainPin, LOW);
+   shiftOut(dataMainPin, clockPinMain, LSBFIRST, Lights);
+   digitalWrite(latchMainPin, HIGH);
+}
